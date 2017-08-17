b9057664cd829bcec75b8d335042bbaceaead382
Matthew Renn <rennmatthewp@gmail.com>
2017-08-17 16:42:36 -0600

    get user guess and display it

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
+//Event Listeners
+
 document.querySelector('#submit-button').addEventListener('click', function(e) {
   e.preventDefault();
   var userGuess = document.getElementById('user-guess').value;
+  document.getElementById('last-guess').innerHTML = userGuess;
 });
+
+//Functions
 
 function randomNumber(min, max) {
   min = Math.ceil(min);
