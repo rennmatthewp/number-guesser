0cef38996f94bfdf18ef87be729bbc60791eec58
Matthew Renn <rennmatthewp@gmail.com>
2017-08-18 10:33:23 -0600

    make global var random number. begin writing comparison function.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
+var randomNumber = randomNumber(0, 100);
+console.log(randomNumber);
 //Event Listeners
 
 document.querySelector('#submit-button').addEventListener('click', function(e) {
   e.preventDefault();
   var userGuess = document.getElementById('user-guess').value;
+  document.getElementById('user-guess').value = ''
   document.getElementById('last-guess').innerHTML = userGuess;
+  checkGuess(userGuess)
 });
 
 //Functions
@@ -13,3 +17,14 @@ function randomNumber(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() *  (max - min + 1)) + min;
 }
+
+function checkGuess(guess) {
+  var feedback = document.getElementById('guess-feedback').innerHTML
+  if (guess < randomNumber){
+    guessFeedback = 'That is too low'
+  };
+  if (guess > randomNumber){
+    guessFeedback = 'That is too high'
+  };
+  return guessFeedback = 'BOOM!'
+},
